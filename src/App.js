19aa22316f1e28a0a807973dedaa3cebdc61613e
Aleksander Kruk <kruk.aleksander@hotmail.com>
2021-08-18 13:36:33 +0200

    create routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Galery from "./components/Galery";
+import Contact from "./components/Contact";
 const App = () => {
   return (
-    <div>
-      <h1>Kociasiec.pl</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Kociasiec.pl</h1>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/onas">
+          <About />
+        </Route>
+        <Route exact path="/galeria">
+          <Galery />
+        </Route>
+        <Route exact path="/kontakt">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
